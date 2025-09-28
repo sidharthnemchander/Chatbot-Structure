1e1dbdab891e182b4e2977d61836951409d6e104
sidharthnemchander <sidharthchander62@gmail.com>
2025-09-28 20:56:58 +0530

    Scraping draft 1

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
-# main.py
+from scraping import scrape_website 
 
-from scraping import scrape_website # Import our new function
-
-# --- Project 1: Scrape a Wikipedia Article ---
+"""This is just a scraping example """
 print("--- Scraping Wikipedia ---")
 wiki_url = "https://en.wikipedia.org/wiki/Large_language_model"
-# Call the function with the URL and the key for the Wikipedia config
 wiki_docs = scrape_website(url=wiki_url, site_key="wikipedia")
 
 if wiki_docs:
-    # Print the first 500 characters of the scraped content
     print(wiki_docs[0].page_content[:500])
 else:
     print("Failed to scrape Wikipedia page.")
 
+#Enter the blog url u want to scrap from and check the site_key to find the correct format of html tag scraping
 
-print("\n" + "="*50 + "\n")
-
-
-# --- Project 2: Scrape Lilian Weng's Blog ---
 print("--- Scraping Lilian Weng's Blog ---")
 blog_url = "https://lilianweng.github.io/posts/2023-06-23-agent/"
-# Reuse the exact same function with a different URL and site_key
 blog_docs = scrape_website(url=blog_url, site_key="lilianweng")
 
 if blog_docs:
